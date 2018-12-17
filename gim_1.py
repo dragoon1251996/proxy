cbc35a095f85b9309de1fa712168c1059c44d73b
Whiyew <vuonguet@gmail.com>
2018-12-17 20:53:58 +0700

    Initial commit

---
+++ gim_1.py
@@ -0,0 +1,40 @@
+
+import time
+import re
+import asyncio
+from proxybroker import Broker
+import redis
+r = redis.Redis(host='localhost', port=6379, db=0)
+async def show(proxies):
+    while True:
+        proxy = await proxies.get()
+        if proxy is None: break
+        key=str(proxy.host)+":"+str(proxy.port)
+        protocol=re.findall("\[(.*?)\]",str(proxy))[0]
+        if "HTTP:" in protocol:
+            protocol=re.findall(":(.*?):",":"+protocol)
+        else:
+            protocol=[x.strip() for x in protocol.split(",")]
+        value={"host":str(proxy.host), "port":str(proxy.port),"protocol":protocol,"country":"US"}
+        r.set(str(key), value)
+        print(value)
+
+
+
+
+
+while True:
+    try:
+        proxies = asyncio.Queue()
+        broker = Broker(proxies)
+        tasks = asyncio.gather(
+            broker.find(types=["HTTP","HTTPS",'SOCKS4', 'SOCKS5'], countries= "US",limit=100),
+            show(proxies))
+        loop = asyncio.get_event_loop()
+        loop.run_until_complete(tasks)
+    except:
+        pass
+    else:
+        print("x")
+
+
